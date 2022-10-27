18c4277bff1564e677e5baea87394b38aec87438
anthonymorton <anthonymorton760@gmail.com>
2022-10-26 20:53:44 -0700

    abandoned scrapy. Using nodejs scraper now. Getting all information from weather page

--- scraper.js
+++ scraper.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const {spawn} = require('child_process');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 router.get('/', (req, res) => {
  
@@ -20,6 +22,42 @@ router.get('/', (req, res) => {
   res.send(dataToSend)
   });
 
+});
+
+router.get('/scraper', async (req, res) => {
+  const url = 'https://forecast.weather.gov/MapClick.php?lat=36.2424&lon=-115.042';
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const temperature = $('p.myforecast-current-lrg').text();
+
+    // console.log(weatherRows.children)
+    const weatherRows = []
+    $('tr > td').each((_idx, el) => {
+      const info = $(el).text()
+      weatherRows.push(info)
+    })
+    const weatherInfo = {
+      'temperature': temperature.slice(0,2),
+      'humidity': weatherRows[1].slice(0,2),
+      'windspeed': weatherRows[3].replace(/\D/g, ""),
+      'barometer': weatherRows[5].slice(0,5)
+    }
+    console.log(weatherRows)
+    // for row in weatherRows:
+        // title = row.css('b::text').get()
+        // if title == 'Humidity':
+        //     item['humidity'] = row.css('td::text').get()
+        // if title == 'Wind Speed':
+        //     item['windspeed'] = row.css('td::text').get()
+        // if title == 'Barometer':
+        //     item['barometer'] = row.css('td::text').get()
+
+    res.send(weatherInfo)
+    
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 module.exports = router;
